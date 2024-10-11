616954081867063b6e0fe0f6e3507d0b1d9c0416
17shrey <157376432+17shrey@users.noreply.github.com>
2024-10-11 12:40:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const dotenv = require('dotenv');
 const mongoose = require ('mongoose');
 const express = require ("express");
 app=express();
+app.use(cors(
+  {
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+    
+  }
+  ));
 
 app.use(express.json());
 require('./db/dbconnection')
